2cfcc3a503971e07acc0f5fe393c45ecb25b3f76
Mariana Farinha <mariana.farinha@nexthink.com>
2025-06-11 20:43:36 +0200

    add initial config

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,32 @@
-import './App.css';
+import { fetchPlayers } from "./api";
+import "./App.css";
+import teams from "./teams";
 
 function App() {
+  function handleChange(e) {
+    const teamId = e.target.value;
+    console.log(teamId);
+    fetchPlayers(teamId);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <img src="ball.webp" className="App-logo" alt="logo" />
+        <h1 className="App-mainTitle">NBA APP</h1>
       </header>
+      <main className="App-main">
+        <select name="team" onChange={handleChange} defaultValue="">
+          <option value="" disabled hidden>
+            Select an option
+          </option>
+          {teams.map((team) => (
+            <option value={team.id} key={team.id}>
+              {team.name}
+            </option>
+          ))}
+        </select>
+      </main>
     </div>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // When your frontend calls `/api`, it'll proxy to Sportradar
+      "/api": {
+        target: "https://api.sportradar.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: true,
+      },
+    },
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
-})
+});
